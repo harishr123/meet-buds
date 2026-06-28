34f5942e8ca6b4f2d685c44acfbe9f7ab7b6bcf1
nus-cs25 <partrag48@gmail.com>
2026-06-29 02:26:39 +0800

    Revert "feat: join/rsvp functionality"
    This reverts commit 92f4a35d6b3a7e0ed2587f851cb3c108be81316b.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import '../services/auth_service.dart';
 import '../feed_screen.dart';
 import 'login_screen.dart';
-import 'profile_screen.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-
-  @override
   Widget build(BuildContext context) {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-
-    final screens = [
-      const FeedScreen(),
-      ProfileScreen(userId: uid),
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meet Buddies'),
@@ -41,23 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
-      body: screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (i) => setState(() => _currentIndex = i),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
-            label: 'Feed',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            activeIcon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
+      body: const FeedScreen(),
     );
   }
 }
